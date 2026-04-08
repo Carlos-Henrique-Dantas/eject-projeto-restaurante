0dadb0ecd6276e0c63516854e27381c0d2dc973e
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2026-04-07 23:42:12 -0300

    Ajustes e feito a base principal dos blog(back-end)

--- RestauranteAPOLLO/urls.py
+++ RestauranteAPOLLO/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import ReservaCreateView, ReservaListView, ReservaCancelView, ReservaDeleteView
+from . import views
+from .views import PostListView, PostDetailView
+
 
 urlpatterns = [
     path('reservas/nova/', ReservaCreateView.as_view(), name='nova_reserva'),
     path('reservas/', ReservaListView.as_view(), name='minhas_reservas'),
     path('reservas/<int:pk>/cancelar/', ReservaCancelView.as_view(), name='cancelar_reserva'),
     path('reservas/<int:pk>/deletar/', ReservaDeleteView.as_view(), name='deletar_reserva'),
+    path('', views.home, name='home'),
+    path('blog/', PostListView.as_view(), name='lista_posts'),
+    path('blog/<int:pk>/', PostDetailView.as_view(), name='post_detalhe'),
 ]
