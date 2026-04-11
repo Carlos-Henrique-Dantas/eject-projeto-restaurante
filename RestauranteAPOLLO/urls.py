3c64305859848fb5a36955c53fcad2cb274eec50
Carlos-Henrique-Dantas <carloshenriquedeoliveira1525@gmail.com>
2026-04-11 17:25:23 -0300

    Configs finais

--- RestauranteAPOLLO/urls.py
+++ RestauranteAPOLLO/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('reservas/<int:pk>/cancelar/', ReservaCancelView.as_view(), name='cancelar_reserva'),
     path('reservas/<int:pk>/deletar/', ReservaDeleteView.as_view(), name='deletar_reserva'),
     path('', views.home, name='home'),
-    path('blog/', PostListView.as_view(), name='lista_posts'),
+    path('', PostListView.as_view(), name='blog'),
+    path('', PostListView.as_view(), name='sobre_nos'),
     path('blog/<int:pk>/', PostDetailView.as_view(), name='post_detalhe'),
 ]
